d966f3a4683af697b4f55b6568282b6dcdd194e6
123456 <1443243401@qq.com>
2016-07-13 09:44:25 +0800

    cleaned gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -245,28 +245,3 @@ gulp.task('express', function() {
   app.listen(4000, '0.0.0.0');
 });
 
-
-
-
-// Single main.js demo
-// This method makes it easy to use common bundling options in different tasks
-function bundle (bundler) {
-
-    // Add options to add to "base" bundler passed as parameter
-    bundler
-      .bundle()                                                        // Start bundle
-      .pipe(source(config.js.src))                        // Entry point
-      .pipe(buffer())                                               // Convert to gulp pipeline
-      .pipe(rename(config.js.outputFile))          // Rename output from 'main.js'
-                                                                              //   to 'bundle.js'
-      .pipe(sourceMaps.init({ loadMaps : true }))  // Strip inline source maps
-      .pipe(sourceMaps.write(config.tasks.js.mapDir))    // Save source maps to their
-                                                                                      //   own directory
-      .pipe(gulp.dest(config.js.outputDir))        // Save 'bundle' to build/
-      .pipe(livereload());                                       // Reload browser if relevant
-}
-
-
-
-
-
